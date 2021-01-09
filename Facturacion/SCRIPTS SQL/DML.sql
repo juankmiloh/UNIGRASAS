531bc65810b805cee7635a4eb914af62b1fbce23
juankmiloh <juankmiloh@hotmail.com>
2021-01-09 15:18:27 -0500

    Se ajustan archivos para despliegue en AZURE

--- Facturacion/SCRIPTS SQL/DML.sql
+++ Facturacion/SCRIPTS SQL/DML.sql
@@ -29,19 +29,18 @@ INSERT INTO TIPO_PERSONA(NOMBRE) VALUES ('Persona jurídica');
 INSERT INTO `UNIGRASAS`.`EMPRESA` (`IDTIPO_PERSONA`, `NIT`, `DIGITO_VERIFICACION`, `RAZON_SOCIAL`, `DIRECCION`, `PAIS`, `CIUDAD`, `CIIU`, `MATRICULA`)
 VALUES (2, 830090675, 7, 'UNIGRASAS COLOMBIA S.A.S', 'Carrera 97 # 24c-51', 'Colombia', 'Bogotá', '4631', '01120659');
 
+----------------------------
+-- DML TABLA ESTADO
+----------------------------
+INSERT INTO `UNIGRASAS`.`ESTADO` (`NOMBRE`) VALUES ('En curso');
+INSERT INTO `UNIGRASAS`.`ESTADO` (`NOMBRE`) VALUES ('Pagada');
+INSERT INTO `UNIGRASAS`.`ESTADO` (`NOMBRE`) VALUES ('Vencida');
 
 ----------------------------
 -- DML TABLA USUARIO
 ----------------------------
 INSERT INTO `UNIGRASAS`.`USUARIO` (`NOMBRE`, `APELLIDO`, `IDGENERO`, `NICKNAME`, `DESCRIPCION`, `IDROL`, `AVATAR`, `CONTRASENA`, `TOKEN`, `IDENTIFICACION`, `IDEMPRESA`, `TELEFONO`, `EMAIL`)
 VALUES ('Nardia', 'Infante', 1, 'nardia', 'Administrador del sistema', 1, 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif', '123456', 'nardia-token', 830090675, 1, 3507259492, 'nardia.unigrasas2015@gmail.com');
-
-
-----------------------------
--- DML TABLA CLIENTE
-----------------------------
-INSERT INTO `UNIGRASAS`.`CLIENTE` (`IDTIPO_PERSONA`, `NIT`, `NOMBRE`, `EMAIL`, `TELEFONO`, `F_REGISTRO`)
-VALUES (1, 901130142, 'PASTELERIA GOURMET MEGAPAN S.A.S', 'cfavilab@gmail.com', 3102821602, CURRENT_TIMESTAMP);
 
 
 ----------------------------
@@ -58,20 +57,18 @@ INSERT INTO `UNIGRASAS`.`MEDIO_PAGO` (`NOMBRE`) VALUES ('Transferencia débito b
 INSERT INTO `UNIGRASAS`.`MEDIO_PAGO` (`NOMBRE`) VALUES ('Consignación bancaria');
 INSERT INTO `UNIGRASAS`.`MEDIO_PAGO` (`NOMBRE`) VALUES ('PSE');
 
+----------------------------
+-- DML TABLA CLIENTE
+----------------------------
+INSERT INTO `UNIGRASAS`.`CLIENTE` (`IDTIPO_PERSONA`, `NIT`, `NOMBRE`, `EMAIL`, `TELEFONO`, `F_REGISTRO`)
+VALUES (1, 901130142, 'PASTELERIA GOURMET MEGAPAN S.A.S', 'cfavilab@gmail.com', 3102821602, CURRENT_TIMESTAMP);
+
 
 ----------------------------
 -- DML TABLA ITEM
 ----------------------------
 INSERT INTO `UNIGRASAS`.`ITEM` (`COD_ITEM`, `NOMBRE`, `PRECIO`, `DESCRIPCION`)
 VALUES (10000, 'MARGARINA INDUSTRIAL AVELLANA T.F', 1, '');
-
-
-----------------------------
--- DML TABLA ESTADO
-----------------------------
-INSERT INTO `UNIGRASAS`.`ESTADO` (`NOMBRE`) VALUES ('En curso');
-INSERT INTO `UNIGRASAS`.`ESTADO` (`NOMBRE`) VALUES ('Pagada');
-INSERT INTO `UNIGRASAS`.`ESTADO` (`NOMBRE`) VALUES ('Vencida');
 
 
 ----------------------------
