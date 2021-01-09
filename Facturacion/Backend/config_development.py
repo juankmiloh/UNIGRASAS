38dfc8619d2f76e7c809338cde865bb05a96ebf6
juankmiloh <juankmiloh@hotmail.com>
2021-01-09 15:23:10 -0500

    Update name database UNIGRASAS

--- Facturacion/Backend/config_development.py
+++ Facturacion/Backend/config_development.py
@@ -4,4 +4,4 @@ DEBUG = True
 PUBLIC = False
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@localhost:5432/procesosDIEG'
-SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@localhost:3306/unigrasas'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@localhost:3306/UNIGRASAS'
--- Facturacion/Backend/config_production.py
+++ Facturacion/Backend/config_production.py
@@ -3,4 +3,4 @@ import os
 DEBUG = False
 PUBLIC = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@localhost:3306/unigrasas'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@localhost:3306/UNIGRASAS'
